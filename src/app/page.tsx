fb2cf0303a16c9ce9ef7d71d47aacf7226e698bb
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-12-12 18:03:00 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Brand from '@/components/Brand'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Hero1 from '@/components/Hero1'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+
+
+export default function (){
+  return (
+    <div>
+      <Header/>
+      <Navbar/>
+      <Hero/>
+      <Hero1/>
+      <Brand/>
+      
+      
+    </div>
+  )
+}
